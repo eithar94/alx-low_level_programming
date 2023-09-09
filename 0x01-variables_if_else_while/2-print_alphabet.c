eac07ecdd612fb3d973679599c44885ff7556cbb
eithar94 <eithar.salah6@gmail.com>
2023-09-08 17:54:14 -0700

    var, if and while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,26 +2,14 @@
 
 int main(void)
 {
-    /* Declare a variable and initialize it with the first member of 
-       the range [a - z] 
-    */
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
 
-    char alphabet = 'a';
-
-     /* check to see if the current value of your variable is less than 
-         or the same as the last member (z) of the given range. While 
-         the value is less, go ahead and print the value 
-      */
-
-    while (alphabet <= 'z')
-    {
-        // print the value of the variable 
-        putchar(alphabet);
-
-        // increment the variable (ie: a becomes b)
-        alphabet++;
-    }
-
-    putchar('\n');
-    return (0);
+        /* increment the variable (ie: a becomes b)*/
+       		alphabet++;
+	}
+	putchar('\n');
+        return (0);
 }
