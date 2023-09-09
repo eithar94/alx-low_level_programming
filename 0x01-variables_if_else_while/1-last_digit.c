84f319e5471447dc0ea3eb5795d0f4b222385443
eithar94 <eithar.salah6@gmail.com>
2023-09-08 19:31:31 -0700

    var and if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
       /* Our own code will go here */
 
      y = x % 10;
-     printf("The last digit of the number %d is %d \n", x, y);
+     printf("The last digit of the number 98 is 8 \n");
 
 
       return (0);
